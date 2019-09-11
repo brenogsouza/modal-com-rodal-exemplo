aba726436d5c11eec2048498f48299b4c246259c
Brenodev <breno.souza.developer@gmail.com>
2019-09-11 18:33:53 -0300

    criado aplicação com exemplos de transição com modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Rodal from './Rodal';
+import './index.css';
+import './Rodal.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            visible: false,
+            animation: 'zoom'
+        }
+    }
+
+    show (animation) {
+        this.setState({
+            animation,
+            visible: true
+        });
+    }
+
+    hide () {
+        this.setState({ visible: false });
+    }
+
+    render () {
+
+        let types = ['zoom', 'fade', 'flip', 'door', 'rotate', 'slideUp', 'slideDown', 'slideLeft', 'slideRight'];
+        let buttons = types.map((value, index) => {
+            let style = {
+                animationDelay       : index * 100 + 'ms',
+                WebkitAnimationDelay : index * 100 + 'ms'
+            };
+            return (
+                <button key={index} className="btn scale" onClick={this.show.bind(this, value)} style={style}>
+                    {value}
+                </button>
+            )
+        });
+
+        return (
+            <div className="wrap">
+                <div className="container" style={{ paddingTop: (window.innerHeight - 440) / 2 }}>
+                    <h1 className="title scale">Modalzinho</h1>
+                    <h3 className="intro scale">Teste</h3>
+                    <div className="btn-area">{buttons}</div>
+                </div>
+                <Rodal visible={this.state.visible}
+                       onClose={this.hide.bind(this)}
+                       animation={this.state.animation}
+                >
+                    <div className="header">Rodal</div>
+                    <div className="body">Teste.</div>
+                    <button className="rodal-confirm-btn" onClick={this.hide.bind(this)}>ok</button>
+                    <button className="rodal-cancel-btn" onClick={this.hide.bind(this)}>close</button>
+                </Rodal>
+            </div>
+        )
+    }
 }
-
-export default App;
+export default App
